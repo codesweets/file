2a5e40e378b5a0a719a72fa11c15862137383539
Trevor Sundberg <tsundberg@s2.systems>
2019-07-11 09:18:36 -0700

    Fix how we include ourself in test

---
+++ postinstall.sh
@@ -0,0 +1,3 @@
+set -e
+mkdir -p './node_modules/@codesweets'
+ln -s -f -n '../..' './node_modules/@codesweets/file'
--- test.sh
+++ test.sh
@@ -1,8 +1,5 @@
 set -e
 cd test
 rm -rf ./bin
-mkdir -p "../node_modules/@codesweets"
-ln -s -f -n "../.." "../node_modules/@codesweets/file"
 tsc
 node ./bin/test.js
-unlink "../node_modules/@codesweets/file"
